b8afc48f677cb3e287d925da5bb6e377003a94f2
liaoyinglong <vigossliao@gmail.com>
2017-11-12 03:00:32 +0800

    修改为ts文件

--- src/common/ts/store.ts
+++ src/common/ts/store.ts
@@ -1,28 +1,27 @@
-export function saveToLocal(id, key, value) {
+export function saveToLocal(id: string, key: string, value: string) {
   let seller = window.localStorage.__seller__
   if (!seller) {
     seller = {}
     seller[id] = {}
   } else {
-    // localStorage存储的是一段字符串
     seller = JSON.parse(seller)
     if (!seller[id]) {
-      sell[id] = {}
+      seller[id] = {}
     }
   }
   seller[id][key] = value
   window.localStorage.__seller__ = JSON.stringify(seller)
 }
 
-export function loadFromLocal(id, key, def) {
-  let seller = window.localStorage.__seller__
+export function loadFromLocal(id: string, key: string, def: string) {
+  let seller: string = window.localStorage.__seller__
   if (!seller) {
     return def
   }
-  let sellerID = JSON.parse(seller)[id]
+  let sellerID: string = JSON.parse(seller)[id]
   if (!sellerID) {
     return def
   }
-  let ret = sellerID[key]
+  let ret: string = sellerID[key]
   return ret || def
-} 
+}
