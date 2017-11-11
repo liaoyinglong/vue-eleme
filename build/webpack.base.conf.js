6bf3df347587cdc5415de1b0758c7b4ae44dfbd2
liaoyinglong <vigossliao@gmail.com>
2017-11-12 00:09:06 +0800

    配置ts环境

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -17,7 +17,7 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.ts', '.js', '.vue', '.json'],
     alias: {
       vue$: 'vue/dist/vue.esm.js',
       src: path.resolve(__dirname, '../src'),
@@ -27,6 +27,14 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+      },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
